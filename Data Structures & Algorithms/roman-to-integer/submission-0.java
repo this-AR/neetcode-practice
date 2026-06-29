ca132d0d98820e96edf046711a9ea990f721f9dc
ADITYA RAI <adityarai735@gmail.com>
2026-06-29 13:44:10 +0530

    Add: roman-to-integer - submission-0

---
+++ Data Structures & Algorithms/roman-to-integer/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int romanToInt(String s) {
+        int ans = 0;
+        int prev = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int curr = value(s.charAt(i));
+
+            if (curr < prev) {
+                ans -= curr;
+            } else {
+                ans += curr;
+            }
+
+            prev = curr;
+        }
+
+        return ans;
+    }
+
+    private int value(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
+}
